90a948b333d77b6b8bbff944c0afa4fd667349e8
DeL00M <del00m@mail.ru>
2016-10-11 16:35:55 +0300

    Draw not null field blocks

--- Game.java
+++ Game.java
@@ -2,14 +2,12 @@ import java.awt.*;
 
 public class Game {
     GameWindow gameWindow;
-    GameField gameField;
+    static GameField gameField;
     final static int BLOCK_SIZE = 20;
     final static int WINDOW_WIDTH = 640;
     final static int WINDOW_HEIGHT = 480;
     final static int WINDOW_TOP = 400;
     final static int WINDOW_LEFT = 200;
-
-    static Snake snake = new Snake(Color.CYAN);
 
     public static void main(String[] args) {new Game().play();}
 
@@ -17,9 +15,14 @@ public class Game {
         gameWindow = new GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TOP, WINDOW_LEFT);
         gameField = new GameField(gameWindow.getWidth()/BLOCK_SIZE, gameWindow.getHeight()/BLOCK_SIZE, BLOCK_SIZE, Color.BLACK);
         gameWindow.add(gameField);
+        gameField.cells[0][0] = new Block(0, 0, BLOCK_SIZE, Color.WHITE);
     }
 
     public static void Paint(Graphics g) {
-        snake.draw(g);
+        for (Block[] i: gameField.cells) {
+            for (Block j: i) {
+                if (j != null) j.draw(g);
+            }
+        }
     }
 }
