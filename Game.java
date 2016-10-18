7249769134a9abdaa353b6dcd9f58fad2f3ee235
DeL00M <del00m@mail.ru>
2016-10-18 15:03:16 +0300

    Default window position now on center

--- Game.java
+++ Game.java
@@ -6,13 +6,16 @@ public class Game {
     final static int BLOCK_SIZE = 20;
     final static int WINDOW_WIDTH = 640;
     final static int WINDOW_HEIGHT = 480;
-    final static int WINDOW_TOP = 400;
-    final static int WINDOW_LEFT = 200;
+    static int WINDOW_TOP;
+    int WINDOW_LEFT;
 
     public static void main(String[] args) {new Game().play();}
 
     private void play() {
-        gameWindow = new GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TOP, WINDOW_LEFT);
+        Dimension screen = Toolkit.getDefaultToolkit ().getScreenSize ();
+        WINDOW_TOP = screen.height / 2 - WINDOW_HEIGHT / 2;
+        WINDOW_LEFT = screen.width / 2 - WINDOW_WIDTH / 2;
+        gameWindow = new GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_LEFT, WINDOW_TOP);
         gameField = new GameField(gameWindow.getWidth()/BLOCK_SIZE, gameWindow.getHeight()/BLOCK_SIZE, BLOCK_SIZE, Color.BLACK);
         gameWindow.add(gameField);
         gameField.cells[0][0] = new Block(0, 0, BLOCK_SIZE, Color.WHITE);
