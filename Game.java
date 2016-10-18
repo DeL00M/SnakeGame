011d9b951e2857ab731a9aa4579d9730ba4007d6
DeL00M <del00m@mail.ru>
2016-10-18 15:19:14 +0300

    Another way to center window

--- Game.java
+++ Game.java
@@ -6,17 +6,16 @@ public class Game {
     final static int BLOCK_SIZE = 20;
     final static int WINDOW_WIDTH = 640;
     final static int WINDOW_HEIGHT = 480;
-    static int WINDOW_TOP;
-    int WINDOW_LEFT;
+    int WINDOW_TOP = 0;
+    int WINDOW_LEFT = 0;
 
     public static void main(String[] args) {new Game().play();}
 
     private void play() {
-        Dimension screen = Toolkit.getDefaultToolkit ().getScreenSize ();
-        WINDOW_TOP = screen.height / 2 - WINDOW_HEIGHT / 2;
-        WINDOW_LEFT = screen.width / 2 - WINDOW_WIDTH / 2;
         gameWindow = new GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_LEFT, WINDOW_TOP);
-        gameField = new GameField(gameWindow.getWidth()/BLOCK_SIZE, gameWindow.getHeight()/BLOCK_SIZE, BLOCK_SIZE, Color.BLACK);
+        gameWindow.setLocationRelativeTo(null); //center window
+        gameField = new GameField(gameWindow.getWidth() / BLOCK_SIZE, gameWindow.getHeight() / BLOCK_SIZE,
+                BLOCK_SIZE, Color.BLACK);
         gameWindow.add(gameField);
         gameField.cells[0][0] = new Block(0, 0, BLOCK_SIZE, Color.WHITE);
     }
