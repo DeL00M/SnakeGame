a6c4483084adbf343ff7489d5e1fc3f4a94ecdd4
Del00M <del00m@mail.ru>
2016-10-19 07:07:46 +0300

    Refactor Line count to length

--- Line.java
+++ Line.java
@@ -6,8 +6,8 @@ public class Line extends Shape{
 
     }
 
-    public Line(int top, int left, int count, Directions direction, Color color) {
-        for (int i = 0; i < count; i++) {
+    public Line(int top, int left, int length, Directions direction, Color color) {
+        for (int i = 0; i < length; i++) {
             this.getBlocks().add(new Block(left + (i * (direction.horizontal * Game.BLOCK_SIZE)),
                     top + (i * (direction.vertical * Game.BLOCK_SIZE)), Game.BLOCK_SIZE, color));
         }
